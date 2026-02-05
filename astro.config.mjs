8f709e4460da4117a074495010a21791ec633425
Mark Bennett <mark@burmis.ca>
2026-02-05 11:05:41 -0700

    Configure Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.markbennett.ca',
   integrations: [mdx(), sitemap(), tailwind()],
   output: 'static',
+
   build: {
     format: 'directory',
   },
+
+  adapter: cloudflare(),
 });
